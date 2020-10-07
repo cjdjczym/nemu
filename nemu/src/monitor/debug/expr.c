dd28bb68f73e5114777ab4f198b3402d7289b65b
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-07 10:42:51 +0000

    >  compile NEMU 3019244334 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:42:51 up  3:07,  1 user,  load average: 1.00, 0.67, 0.55 5e2a6abb3d21df8627153f03b6585b79d4a47ea9

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -157,7 +157,10 @@ uint32_t eval(int p, int q) {
         if (tokens[p].type == NUM)
             sscanf(tokens[p].str, "%d", &number);
         return number;
-    } else if (check_parentheses(p, q)) return eval(p + 1, q - 1);
+    } else if (check_parentheses(p, q)) {
+        printf("%c%c",tokens[p].type,tokens[q].type);
+        return eval(p + 1, q - 1);
+    }
     else {
         int main_op = dominant_operator(p, q);
         uint32_t first = eval(p, main_op - 1);
