cc86f7e62a3c1f656d8e3685f6b3c8e8208d0f93
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-17 18:41:04 +0800

    >  compile NEMU 3019244334 cuijun Linux bot-illya 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:41:04 up  4:28,  1 user,  load average: 0.23, 0.26, 0.26 ae089de27a7123b85a90e0cb4213c8fad5c6f6a6

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -5,7 +5,6 @@
 uint32_t lnaddr_read(lnaddr_t, size_t);
 
 void load_sreg(uint8_t sreg, uint16_t sel) {
-	printf("sreg: %d\n", sreg);
 	assert(sreg < 6);
 	cpu.sreg[sreg].val = sel;
 	assert(cpu.sreg[sreg].ti == 0);		// only GDT is simulated
