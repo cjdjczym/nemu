8d3b695b569ee0ddb594bd99a0f5a8abeb9f14f9
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-17 19:11:06 +0800

    >  compile NEMU 3019244334 cuijun Linux bot-illya 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:11:06 up  4:58,  1 user,  load average: 0.83, 0.40, 0.31 777a68ca6764927a91d8d7aa6b1ef956f0ae215f

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -85,7 +85,6 @@ void swaddr_write(swaddr_t addr, size_t len, uint32_t data, uint8_t sreg) {
 #ifdef DEBUG
 	assert(len == 1 || len == 2 || len == 4);
 #endif
-	printf("write\n");
 	lnaddr_t lnaddr = segment_translate(addr, sreg);
 	lnaddr_write(lnaddr, len, data);
 }
